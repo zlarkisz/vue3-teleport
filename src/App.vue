eae7d6a3551278a5700ee8652ab1569061d3ea61
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-08 23:45:00 +0300

    create first vue3 teleport

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+
+  <p>Welcome...</p>
+
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template #links>
+        <a href="#">sign in now</a>
+        <a href="#">more info</a>
+      </template>
+
+      <h1>Sign up for the Giveaway</h1>
+      <p>Grab your ninja swag for half price !</p>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up for the newsletter</h1>
+      <p>for updates and promocodes !</p>
+    </Modal>
+  </teleport>
+
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModalTwo">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    Modal,
+  },
+
+  data() {
+    return {
+      title: 'My first Vue3 app :)',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
+  },
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Modal.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Modal',
+
+    props: {
+      theme: {
+        type: String,
+        default: ''
+      }
+    },
+
+    methods: {
+      closeModal() {
+        this.$emit('close')
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  height: 100%;
+  width: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+
+  &.sale {
+    background: crimson;
+    color: white;
+
+    h1 {
+      color: white;
+    }
+
+    .actions {
+      &::v-deep a {
+        color: white;
+      }
+    }
+  }
+
+  .actions {
+    text-align: center;
+    margin: 30px 0 10px;
+
+    &::v-deep a {
+      color: #333;
+      padding: 8px;
+      border: 1px solid #eee;
+      border-radius: 4px;
+      text-decoration: none;
+      margin: 10px;
+    }
+  }
+}
+
+h1 {
+  color: #03cfb4;
+}
+
+p {
+  font-style: normal;
+}
+</style>
